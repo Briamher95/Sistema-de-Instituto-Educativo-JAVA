d83d8ed983a9f58649cf2aedb616458dee3dc806
Fausto <nicolasfausto5@gmail.com>
2026-06-16 00:06:16 -0300

    Creacion e implementacion de la clase Materia, con atributos preestablecidos, constructores y métodos.

--- SistemaInstituto/src/dominio/Materia.java
+++ SistemaInstituto/src/dominio/Materia.java
@@ -1,5 +1,60 @@
 package dominio;
+import enums.EstadoCurso;
 
-public class Materia {
+// La clase Materia hereda atributos y métodos de Curso.
+public class Materia extends Curso {
+
+    private Integer horasSemanales;
+    private String correlativa;
+
+    // Inicializa los atributos heredados llamando al constructor de Curso.
+    public Materia(Integer id,
+                   String nombre,
+                   Integer cupoMaximo,
+                   Integer inscriptos,
+                   EstadoCurso estado,
+                   Integer horasSemanales,
+                   String correlativa) {
+
+        super(id, nombre, cupoMaximo, inscriptos, estado);
+
+        //Inicializar atributos propios
+        this.horasSemanales = horasSemanales;
+        this.correlativa = correlativa;
+    }
+
     
+
+    public Integer getHorasSemanales() {
+        return horasSemanales;
+    }
+
+
+
+    public void setHorasSemanales(Integer horasSemanales) {
+        this.horasSemanales = horasSemanales;
+    }
+
+
+
+    public String getCorrelativa() {
+        return correlativa;
+    }
+
+
+
+    public void setCorrelativa(String correlativa) {
+        this.correlativa = correlativa;
+    }
+
+    //Métodos 
+    
+
+
+    // Implementación obligatoria del método abstracto definido en Curso.
+    @Override
+    public String getModalidad() {
+        return "Materia";
+    }
 }
+
