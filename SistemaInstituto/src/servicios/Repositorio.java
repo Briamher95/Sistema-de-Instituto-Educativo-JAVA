12a46b206039a808a1e2a2c8e71b2d6922dc3794
Briamher95 <bryandhernandez95@gmail.com>
2026-06-18 15:19:25 -0300

    Terminada la serializacion

---
+++ SistemaInstituto/src/servicios/Repositorio.java
@@ -0,0 +1,48 @@
+package servicios;
+
+import dominio.interfaces.IRepositorio;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+
+public class Repositorio implements IRepositorio {
+    private File archivo;
+
+    public Repositorio (String nombreArchivo){
+        this.archivo = new File (nombreArchivo);
+    }
+
+
+ 
+    //Convierte el objeto a bytes y lo guarda en el archivo
+    @Override
+    public void guardar(Object obj) {
+        // Oss es la herramienta que escribe objetos en bytes
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo)))
+        {
+            oos.writeObject(obj);
+        } catch (IOException e){
+            System.out.println("Error al guardar el objeto: " + e.getMessage());
+        }
+        
+    }
+    //Reconstruye el objeto a partir de los bytes guardados en el archivo
+    @Override
+    public List<?> consultar() {
+        if (!archivo.exists()) return new ArrayList<>();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))) {
+            //Se castea a lista
+            return (List<?>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error al leer: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+}
